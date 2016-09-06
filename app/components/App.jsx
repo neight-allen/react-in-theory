31b354fff0240c84400d80600f0f6d739432e613
martensonbj <martensonbj@gmail.com>
2016-09-05 21:59:21 -0600

    Refactor with separate component files

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,37 +1,13 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
+const LikesCounter = require('./LikesCounter')
 
-class LikesCounter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {count: props.initialCount};
-  }
-
-  addToLikesCount (num) {
-    this.setState( {count: this.state.count + num } )
-  }
-
+class App extends React.Component {
   render () {
     return (
-      <div className="LikesCounter">
-        <h3>Likes: {this.state.count}</h3>
-        <div className="ActionButtons">
-          <ActionButton text="Like! (+1)" handleClick={this.addToLikesCount.bind(this, 1)} />
-          <ActionButton text="Dislike! (-1)" handleClick={this.addToLikesCount.bind(this, -1)} />
-        </div>
-      </div>
+      <LikesCounter initialCount={0}/>
     )
   }
 }
 
-class ActionButton extends React.Component {
-  render () {
-    return (
-      <button className="ActionButton" onClick={this.props.handleClick}>
-        <span>{this.props.text}</span>
-      </button>
-    )
-  }
-}
-
-ReactDOM.render(<LikesCounter initialCount={0}/>, document.getElementById('application'))
+ReactDOM.render( <App />, document.getElementById('application'))
